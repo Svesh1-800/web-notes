f6b18b97f2910eb9355564e55e8b01f68e62b9f7
Svesh1-800 <edik.khugaev@mail.ru>
2020-11-13 17:48:19 +0300

    created model for notes
    - first version of model Note
    - makemigration and migrate

--- jnotes/notes/models.py
+++ jnotes/notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# модель для создания аттрибутов заметки
+class Note(models.Model):
+    title_note = models.CharField(blank=False, default='NoTitle',max_length=150)
+    category_note = models.CharField(blank=True, default='out', max_length=150)
+    content_body = models.CharField(blank=True, max_length=1000)
+    post_date_note = models.DateField(auto_now_add=True)
