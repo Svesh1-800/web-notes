f98ca1ea6c8cf7158cac83d271cf0ba9938bc7b3
Svesh1-800 <edik.khugaev@mail.ru>
2020-11-13 18:06:59 +0300

    admin
    - creadted superuser
    - register model Note in admin.py

--- jnotes/notes/models.py
+++ jnotes/notes/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 # модель для создания аттрибутов заметки
 class Note(models.Model):
-    title_note = models.CharField(blank=False, default='NoTitle',max_length=150)
+    title_note = models.CharField(blank=False, default='NoTitle', max_length=150)
     category_note = models.CharField(blank=True, default='out', max_length=150)
     content_body = models.CharField(blank=True, max_length=1000)
     post_date_note = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title_note + ' | ' + self.content_body
