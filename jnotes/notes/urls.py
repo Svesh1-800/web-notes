b238925712dc735b3449b183f59b334d449c4aa6
Svesh1-800 <edik.khugaev@mail.ru>
2020-11-15 11:42:55 +0300

    some frontend stuff
    - created 'base.htm' probably with some bugs
    - created 'home.html'
    - new class HomeView in views.py
    - created static directory and path to it in settings.py
    - added path in urls.py

--- jnotes/notes/urls.py
+++ jnotes/notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import test
+from .views import HomeView
 
 urlpatterns = [
-    path('', test)
+    path('', HomeView.as_view(), name='home')
 ]
--- jnotes/notes/views.py
+++ jnotes/notes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Note
 
-def test(request):
-    return render(request,'home.html')
+class HomeView(ListView):
+    model = Note
+    template_name = 'home.html'
+    ordering = ['-post_date_note']
