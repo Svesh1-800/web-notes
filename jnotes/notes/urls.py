aeda6a6a6eab449112c30806e49df48bb890cee3
Svesh1-800 <edik.khugaev@mail.ru>
2020-11-19 17:58:30 +0300

    created NoteDetailView
    - new url address
    - now i know what context_object_name is
    - added href in link 'Jnotes'

--- jnotes/notes/urls.py
+++ jnotes/notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, NoteDetailView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('note/<int:pk>', NoteDetailView.as_view(), name='note-detail'),
 ]
--- jnotes/notes/views.py
+++ jnotes/notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Note
 
@@ -7,3 +7,9 @@ class HomeView(ListView):
     model = Note
     template_name = 'home.html'
     ordering = ['-post_date_note']
+    context_object_name = 'notes_list'
+
+class NoteDetailView(DetailView):
+    model = Note
+    template_name = 'note-detail.html'
+    context_object_name = 'note'
