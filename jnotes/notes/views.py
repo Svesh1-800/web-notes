0890c270b4fe49439206ff9e68fc5d13bd868754
Svesh1-800 <edik.khugaev@mail.ru>
2020-12-14 19:20:02 +0300

    unique names for Category

--- jnotes/notes/views.py
+++ jnotes/notes/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse, reverse_lazy
 
 from .models import Note, Category
 
+
 # основная страница, со всеми заметками
 class HomeView(ListView):
     model = Note
@@ -12,11 +13,13 @@ class HomeView(ListView):
     ordering = ['-date_note']
     context_object_name = 'notes_list'
 
+
 # содержание заметки
 class NoteDetailView(DetailView):
     model = Note
     template_name = 'note-detail.html'
     context_object_name = 'note'
+
 
 # создание заметки
 class NoteAddView(CreateView):
@@ -24,12 +27,14 @@ class NoteAddView(CreateView):
     template_name = 'note-add.html'
     fields = '__all__'
 
+
 # удаление заметки
 class NoteDeleteView(DeleteView):
     model = Note
     template_name = 'note-delete.html'
     success_url = reverse_lazy('home')
     context_object_name = 'note'
+
 
 # Обновление заметки
 class NoteUpdateView(UpdateView):
@@ -44,8 +49,7 @@ def CategoriesList(request):
     context = {
         'categories': data
     }
-    return render(request, 'categories_list.html', context= context)
-
+    return render(request, 'categories_list.html', context=context)
 
 # def OneCategoryList(request, current_category):
 #     category_posts = Note.objects.filter(category_note = current_category)
@@ -55,6 +59,3 @@ def CategoriesList(request):
 # order_by() - отсортировать по определенному полю
 # values(name) - получить  объекты одного поля
 # values_list(name) - получить все объекты одного поля в формате листа
-
-
-
