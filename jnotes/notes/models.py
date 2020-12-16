ebe96f1b56959554f2024e943adbf92ff9c3cca9
Svesh1-800 <edik.khugaev@mail.ru>
2020-12-16 13:00:06 +0300

    updeated comments

--- jnotes/notes/models.py
+++ jnotes/notes/models.py
@@ -1,31 +1,33 @@
 from django.db import models
 from django.urls import reverse
-from django.core.exceptions import ValidationError
 
 
 # модель для создания категорий
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True, editable=True)
 
-# метод возвращает строковое представление
+    # метод возвращает строковое представление
     def __str__(self):
         return self.name
 
+    # Метод, который переводит все в нижний регистр.
+    # Теперь не имеет значения, в каком регистре все вводится
     def clean(self):
         if self.name != self.name.lower():
             self.name = self.name.lower()
 
 
-
 # модель для создания аттрибутов заметки
 class Note(models.Model):
     title_note = models.CharField('Название', blank=False, default='NoTitle', max_length=150)
-    category_note = models.ForeignKey(Category, on_delete= models.CASCADE)
+    category_note = models.ForeignKey(Category, on_delete=models.CASCADE)
     content_note = models.TextField('Содержание', blank=True)
     date_note = models.DateTimeField(auto_now_add=True)
 
+    # при создании заметки пользователя перекидывает на главную страничку
     def get_absolute_url(self):
         return reverse('home')
 
+    # строковое представление
     def __str__(self):
         return self.title_note + ' | ' + self.content_note
